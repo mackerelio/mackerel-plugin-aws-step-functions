1648feded74eaa938439730aecba59cc0e0d089d
Yasuhisa Yoshida <syou6162@gmail.com>
2018-04-18 10:29:38 +0900

    fix import

--- lib/aws-step-functions.go
+++ lib/aws-step-functions.go
@@ -5,12 +5,13 @@ import (
 	"time"
 
 	"errors"
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	mp "github.com/mackerelio/go-mackerel-plugin"
-	"log"
 )
 
 const (
